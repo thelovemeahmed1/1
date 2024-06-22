7681ffbd67d230c1e10cfab58851456c6e8fea87
thelovemeahmed1 <thelovemeahmed1@gmail.com>
2024-06-22 15:12:07 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -43,7 +43,7 @@ firebaseio_link='https://hotmail-4261b-default-rtdb.firebaseio.com'
 
 xvfb = Popen(['Xvfb', ':1', '-screen', '0', '1920x1080x24'])
 os.environ['DISPLAY'] = ':1'
-code_country = '998'
+code_country = '63'
 def eday():
     while True:
         try:
