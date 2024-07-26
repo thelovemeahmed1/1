332bbc954bf2af753c3550aa3a50ba5cf3af907a
thelovemeahmed1 <thelovemeahmed1@gmail.com>
2024-07-26 04:00:49 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,1 +1,27 @@
-gw.dataimpulse.com:823
+from appium import webdriver
+from selenium.webdriver.remote.remote_connection import RemoteConnection
+
+# إعداد القدرات
+desired_caps = {
+    "platformName": "Android",
+    "deviceName": "serveo.net:5551",
+    "automationName": "UiAutomator2",
+    "appPackage": "com.tinder",
+    "appActivity": "com.tinder.launch.internal.activities.LoginActivity",
+    "noReset": True
+}
+
+# عنوان الخادم و المنفذ
+server_url = 'http://localhost:4723/wd/hub'  # تأكد من أن Appium يعمل على هذا المنفذ
+
+# تعيين مهلة الاتصال
+RemoteConnection.set_timeout(60)  # تعيين مهلة الاتصال إلى 60 ثانية
+
+# إنشاء مثيل لمشغل Appium
+driver = webdriver.Remote(server_url, desired_caps)
+
+# تأكد من أن الاتصال تم بنجاح
+print("App launched successfully!")
+
+# يمكنك الآن التفاعل مع التطبيق عبر `driver`
+
